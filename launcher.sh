a6b0b16b6a3793c5ff5dcc6f96bd6243c7683dbf
hieunm18799 <hieunm18799@gmail.com>
2024-01-22 07:10:23 +0700

    fix launch

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,4 @@
 cd /home/pi/Desktop/face
 . /home/pi/Desktop/face/myenv/bin/activate
-python3 /home/pi/Desktop/face/face_recognize_client.py
+/home/pi/Desktop/face/myenv/bin/python3 /home/pi/Desktop/face/face_recognize_client.py
 cd /home/pi
